ff7e79a29e3a0e539e2a9298ad7a2e0be78a654d
mikxingu <michelalvs@gmail.com>
2022-02-07 23:12:09 -0300

    Added User Test Case

---
+++ itau-user/src/test/java/com/itau/api/controller/UsuarioControllerTest.java
@@ -0,0 +1,14 @@
+package com.itau.api.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class UsuarioControllerTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ itau-user/src/test/java/com/itau/api/repository/UsuarioRepositoryTest.java
@@ -0,0 +1,14 @@
+package com.itau.api.repository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class UsuarioRepositoryTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
